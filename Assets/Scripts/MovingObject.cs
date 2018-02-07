09762237859608ad1c091910144701abd9d80a5f
coconut750750 <sourbrandon@gmail.com>
2018-02-06 23:57:55 -0600

    initial commit

---
+++ Assets/Scripts/MovingObject.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityStandardAssets.CrossPlatformInput;
+
+public abstract class MovingObject : MonoBehaviour {
+
+	public LayerMask blockingLayer;
+
+	private Rigidbody2D rb2D;
+	private bool paused = false;
+
+	private const string DOOR = "Door";
+	private const float DOOR_DELAY_SECONDS = 0.02f;
+	private Sprite currentDoorSprite;
+
+	IEnumerator doorDelay(Collider2D door) {
+		paused = true;
+		yield return new WaitForSeconds(DOOR_DELAY_SECONDS);
+		paused = false;
+		door.gameObject.GetComponent<SpriteRenderer> ().sprite = null;
+	}
+
+	void Awake() {
+		rb2D = GetComponent<Rigidbody2D>();
+		rb2D.collisionDetectionMode = CollisionDetectionMode2D.Continuous;
+		rb2D.interpolation = RigidbodyInterpolation2D.Extrapolate;
+		rb2D.freezeRotation = true;
+	}
+
+	// Use this for initialization
+	protected virtual void Start () {
+	}
+
+	protected abstract void FixedUpdate ();
+
+	protected void Move(Vector3 movement) {
+		if (!paused) {
+			rb2D.velocity = movement;
+		} else {
+			rb2D.velocity = new Vector3(0, 0, 0);
+		}
+	}
+
+	protected void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.CompareTag(DOOR)) {
+			StartCoroutine(doorDelay(other));
+			currentDoorSprite = other.gameObject.GetComponent<SpriteRenderer> ().sprite;
+		}
+	}
+
+	protected void OnTriggerExit2D(Collider2D other) {
+		if (other.gameObject.CompareTag(DOOR)) {
+			other.gameObject.GetComponent<SpriteRenderer> ().sprite = currentDoorSprite;
+		}
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MovingObject {
+
+	public float moveSpeed;
+	private float MovementRange = 25f;
+
+	private const string FORWARD = "PlayerForwardAnim";
+	private const string LEFT = "PlayerLeftAnim";
+	private const string BACK = "PlayerBackAnim";
+	private const string RIGHT = "PlayerRightAnim";
+
+	private Animator animator;
+
+	protected override void Start (){
+		animator = GetComponent<Animator> ();
+	}
+
+	protected override void FixedUpdate() {
+		float moveHorizontal = 0;
+		float moveVertical = 0;
+
+		Vector3 movement;
+		#if UNITY_STANDALONE || UNITY_WEBPLAYER
+
+		moveHorizontal = Input.GetAxis ("Horizontal");
+		moveVertical = Input.GetAxis ("Vertical");
+		movement = Vector3.ClampMagnitude(new Vector3(moveHorizontal, moveVertical, 0f), MovementRange);
+		
+		#elif UNITY_IOS || UNITY_ANDROID || UNITY_WP8 || UNITY_IPHONE
+
+		moveHorizontal = CrossPlatformInputManager.GetAxis("Horizontal");
+		moveVertical = CrossPlatformInputManager.GetAxis("Vertical");
+		movement = new Vector3(moveHorizontal, moveVertical, 0f);
+
+		#endif
+		
+		movement *= moveSpeed;
+
+		base.Move(movement);
+
+		if (movement.sqrMagnitude == 0) {
+			return;
+		}
+
+		string currentAnim = animator.GetCurrentAnimatorClipInfo(0)[0].clip.name;
+
+		if (Mathf.Abs (moveVertical) >= Mathf.Abs (moveHorizontal)) {
+			if (moveVertical <= 0) {
+				if (currentAnim != FORWARD) {
+					animator.Play(FORWARD);
+				}
+				animator.Play(FORWARD);
+			} else {
+				if (currentAnim != BACK) {
+					animator.Play(BACK);
+				}
+			}
+		} else {
+			if (moveHorizontal <= 0) {
+				if (currentAnim != LEFT) {
+					animator.Play(LEFT);
+				}
+			} else {
+				if (currentAnim != RIGHT) {
+					animator.Play(RIGHT);
+				}
+			}
+		}
+	}
+}
