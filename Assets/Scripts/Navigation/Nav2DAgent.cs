cbfee8494178468f0ffcbe47f0eff869f4dfe594
Brandon Wang <sourbrandon@gmail.com>
2018-04-26 17:10:29 -0500

    cleaned up nav2d agent

--- Assets/Scripts/Navigation/Nav2DAgent.cs
+++ Assets/Scripts/Navigation/Nav2DAgent.cs
@@ -33,7 +33,6 @@ public class Nav2DAgent : MonoBehaviour{
 	///Will debug the path (gizmos). Disable for performance.
 	public bool debugPath            = true;
 
-
 	///Raised when a new destination is started after path found
 	public event Action OnNavigationStarted;
 	///Raised when the destination is reached
@@ -47,10 +46,11 @@ public class Nav2DAgent : MonoBehaviour{
 
 	private Rigidbody2D rb2d;
 	private Vector2 velocity          = Vector2.zero;
-	private float maxForce            = 100;
 	private int requests              = 0;
 	private List<Vector2> _activePath = new List<Vector2>();
-	private Vector2 _primeGoal        = Vector2.zero;
+	
+	///The current goal of the agent
+	private Vector2 primeGoal        = Vector2.zero;
 
 	private static List<Nav2DAgent> allAgents = new List<Nav2DAgent>();
 
@@ -62,7 +62,9 @@ public class Nav2DAgent : MonoBehaviour{
 
 	///The current active path of the agent
 	public List<Vector2> activePath{
-		get { return _activePath; }
+		get { 
+			return _activePath; 
+		}
 		set
 		{
 			_activePath = value;
@@ -71,14 +73,8 @@ public class Nav2DAgent : MonoBehaviour{
 		}
 	}
 
-	///The current goal of the agent
-	public Vector2 primeGoal{
-		get { return _primeGoal;}
-		set { _primeGoal = value;}
-	}
-
 	///Is a path pending?
-	public bool pathPending{
+	public bool pathPending {
 		get	{ return requests > 0;}
 	}
 
@@ -92,8 +88,7 @@ public class Nav2DAgent : MonoBehaviour{
 
 	///The point that the agent is currenty going to. Returns the agent position if no active path
 	public Vector2 nextPoint{
-		get
-		{
+		get {
 			if (hasPath)
 				return activePath[0];
 			return position;			
@@ -102,32 +97,33 @@ public class Nav2DAgent : MonoBehaviour{
 
 	///The remaining distance of the active path. 0 if none
 	public float remainingDistance{
-		get
-		{
-			if (!hasPath)
+		get {
+			if (!hasPath) {
 				return 0;
-
+			}
 			float dist= Vector2.Distance(position, activePath[0]);
-			for (int i= 0; i < activePath.Count; i++)
+			for (int i= 0; i < activePath.Count; i++) {
 				dist += Vector2.Distance(activePath[i], activePath[i == activePath.Count - 1? i : i + 1]);
-
-			return dist;			
+			}
+			return dist;
 		}
 	}
 
 	///The moving direction of the agent
 	public Vector2 movingDirection{
-		get
-		{
-			if (hasPath)
+		get {
+			if (hasPath) {
 				return velocity.normalized;
-			return Vector2.zero;			
+			}
+			return Vector2.zero;
 		}
 	}
 
 	///The current speed of the agent
 	public float currentSpeed{
-		get {return velocity.magnitude;}
+		get {
+			return velocity.magnitude;
+		}
 	}
 
 	void OnEnable(){ allAgents.Add(this); }
@@ -142,7 +138,6 @@ public class Nav2DAgent : MonoBehaviour{
 
 	///Set the destination for the agent. As a result the agent starts moving. Only the callback from the last SetDestination will be called upon arrival
 	public bool SetDestination(Vector2 goal, Action<bool> callback){
-
 		if (polyNav == null){
 			Debug.LogError("No Nav2D in scene!");
 			return false;
@@ -193,44 +188,42 @@ public class Nav2DAgent : MonoBehaviour{
 		primeGoal = position;
 	}
 
-
 	//the callback from polyNav for when path is ready to use
 	void SetPath(Vector2[] path, bool success){
 		//in case the agent stoped somehow, but a path was pending
-		if (requests == 0)
+		if (requests == 0) {
 			return;
+		}
 
-		requests --;
+		requests--;
 
-		if (!success || path.Length == 0){
+		if (!success || path.Length == 0) {
 			OnInvalid();
 			return;
 		}
 
 		activePath = path.ToList();
-		if (OnNavigationStarted != null)
+		if (OnNavigationStarted != null) {
 			OnNavigationStarted();
+		}
 	}
 
 	//main loop
 	void LateUpdate(){
 
-		if (polyNav == null)
+		if (polyNav == null) {
 			return;
-
+		}
 		//when there is no path just restrict
-		if (!hasPath){
+		if (!hasPath) {
 			Restrict();
 			return;
 		}
 
 		//calculate velocities
-		if (remainingDistance < slowingDistance){
-			
+		if (remainingDistance < slowingDistance) {
 			velocity += Arrive(nextPoint) / mass;
-
 		} else {
-
 			velocity += Seek(nextPoint) / mass;
 		}
 
@@ -241,90 +234,84 @@ public class Nav2DAgent : MonoBehaviour{
 
 		//move the agent
 		rb2d.velocity = velocity;
-		//position = gameObject.transform.position;
 
 		//restrict just after movement
 		Restrict();
 
 		//rotate if must
-		if (rotateTransform){
+		if (rotateTransform) {
 			float rot = -Mathf.Atan2(movingDirection.x, movingDirection.y) * 180 / Mathf.PI;
 			float newZ = Mathf.MoveTowardsAngle(transform.localEulerAngles.z, rot, rotateSpeed * Time.deltaTime);
 			transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y, newZ);
 		}
 
-		if (repath){
+		if (repath) {
 
 			//repath if there is no LOS with the next point
-			if (polyNav.CheckLOS(position, nextPoint) == false)
+			if (polyNav.CheckLOS(position, nextPoint) == false) {
 				Repath();
-
+			}
 			//in case just after repath-ing there is no path
-			if (!hasPath){
+			if (!hasPath) {
 				OnArrived();
 				return;
 			}
 		}
 
-		//Check and remove if we reached a point. proximity distance depends
-		float proximity = (activePath[activePath.Count -1] == nextPoint)? stoppingDistance : 0.05f;
-		if ((position - nextPoint).sqrMagnitude <= proximity){
-
+		//Check and remove if we reached a point. 
+		if ((position - nextPoint).sqrMagnitude <= stoppingDistance){
 			activePath.RemoveAt(0);
 
 			//if it was last point, means the path is complete and no longer have an active path.
-			if (!hasPath){
-			
+			if (!hasPath) {
 				OnArrived();
 				return;
-			
 			} else {
-
 				if (repath){
 					//repath after a point is reached
 					Repath();
 				}
 
-				if (OnNavigationPointReached != null)
+				if (OnNavigationPointReached != null) {
 					OnNavigationPointReached(position);
+				}
 			}
 		}
 
 		//little trick. Check the next waypoint ahead of the current for LOS and if true consider the current reached.
 		//helps for tight corners and when agent has big innertia
-		if (activePath.Count > 1 && polyNav.CheckLOS(position, activePath[1])){
-			activePath.RemoveAt(0);
-			if (OnNavigationPointReached != null)
-				OnNavigationPointReached(position);
-		}
+		// if (activePath.Count > 1 && polyNav.CheckLOS(position, activePath[1])){
+		// 	activePath.RemoveAt(0);
+		// 	if (OnNavigationPointReached != null)
+		// 		OnNavigationPointReached(position);
+		// }
 	}
 
 
 	//recalculate path to prime goal if there is no pending requests
-	void Repath(){
-
-		if (requests > 0)
+	void Repath() {
+		if (requests > 0) {
 			return;
+		}
 
-		requests ++;
+		requests++;
 		polyNav.FindPath(position, primeGoal, SetPath);
 	}
 
 	//stop the agent and callback + message
-	void OnArrived(){
-
+	void OnArrived() {
 		Stop();
 
-		if (reachedCallback != null)
+		if (reachedCallback != null) {
 			reachedCallback(true);
-
-		if (OnDestinationReached != null)
+		}
+		if (OnDestinationReached != null) {
 			OnDestinationReached();
+		}
 	}
 
 	//stop the agent and callback + message
 	void OnInvalid(){
-
 		Stop();
 
 		if (reachedCallback != null)
@@ -337,10 +324,9 @@ public class Nav2DAgent : MonoBehaviour{
 	
 	//seeking a target
 	Vector2 Seek(Vector2 pos){
-
-		Vector2 desiredVelocity= (pos - position).normalized * maxSpeed;
-		Vector2 steer= desiredVelocity - velocity;
-		steer = Truncate(steer, maxForce);
+		Vector2 desiredVelocity = (pos - position).normalized * maxSpeed;
+		Vector2 steer = desiredVelocity - velocity;
+		steer = Truncate(steer, maxSpeed);
 		return steer;
 	}
 
@@ -348,16 +334,16 @@ public class Nav2DAgent : MonoBehaviour{
 	Vector2 Arrive(Vector2 pos){
 
 		var desiredVelocity = (pos - position);
-		float dist= desiredVelocity.magnitude;
+		float dist = desiredVelocity.magnitude;
 
 		if (dist > 0){
-			var reqSpeed = dist / (decelerationRate * 0.3f);
+			var reqSpeed = dist / (decelerationRate);
 			reqSpeed = Mathf.Min(reqSpeed, maxSpeed);
 			desiredVelocity *= reqSpeed / dist;
 		}
 
 		Vector2 steer= desiredVelocity - velocity;
-		steer = Truncate(steer, maxForce);
+		steer = Truncate(steer, maxSpeed);
 		return steer;
 	}
 
