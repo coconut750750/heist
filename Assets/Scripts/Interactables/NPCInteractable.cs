f4eba12ba4834eee8beac35d72ad8c3ee85410a8
Brandon Wang <sourbrandon@gmail.com>
2018-05-27 00:12:20 -0500

    working version of speech bubble

--- Assets/Scripts/Interactables/NPCInteractable.cs
+++ Assets/Scripts/Interactables/NPCInteractable.cs
@@ -7,7 +7,11 @@ public class NPCInteractable : Interactable {
 
     public GameObject hoverNameText;
     private GameObject hoverTextInstance = null;
-    private const float NAME_OFFSET = 0.75f; // In game tile space, not pixel space
+    private Vector3 NAME_OFFSET = new Vector3(0, 0.75f, 0); // In game tile space, not pixel space
+
+    public GameObject speechBubble;
+    private GameObject speechBubbleInstance = null;
+    private Vector3 SPEECH_OFFSET = new Vector3(0, 0, 0);
 
 	private NPC npcObject;
 
@@ -19,19 +23,28 @@ public class NPCInteractable : Interactable {
     void Update () {
         if (hoverTextInstance != null) {
             hoverTextInstance.transform.position = Camera.main.WorldToScreenPoint(
-                                                gameObject.transform.position + new Vector3(0, NAME_OFFSET, 0));
-
+                                                gameObject.transform.position + NAME_OFFSET);
+        }
+        if (speechBubbleInstance != null) {
+            speechBubbleInstance.transform.position = Camera.main.WorldToScreenPoint(
+                                                gameObject.transform.position + NAME_OFFSET);
         }
     }
 	
 	public override void Interact(Player player) {
         // open inventory
-        GameManager.instance.npcDisplayer.Display(npcObject);
+        // GameManager.instance.npcDisplayer.Display(npcObject);
+
+        // greet player
+        speechBubbleInstance = Instantiate(speechBubble);
+
+        speechBubbleInstance.GetComponentInChildren<Text>().text = GetComponent<NPC>().Greet();
+        speechBubbleInstance.transform.SetParent(GameManager.instance.canvas.transform, false);
     }
 
     public override void EnterRange(Player player)
     {
-        hoverTextInstance = Instantiate(hoverNameText, gameObject.transform.position, Quaternion.identity);
+        hoverTextInstance = Instantiate(hoverNameText);
     
         hoverTextInstance.GetComponentInChildren<Text>().text = GetComponent<NPC>().GetName();
         hoverTextInstance.transform.SetParent(GameManager.instance.canvas.transform, false);
