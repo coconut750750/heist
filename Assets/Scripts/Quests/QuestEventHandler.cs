66629bbae3556c91c569b6a8d96478125ee50dab
Brandon Wang <sourbrandon@gmail.com>
2018-06-13 21:05:45 -0500

    CODE preliminary quest event handler

---
+++ Assets/Scripts/Quests/QuestEventHandler.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestEventHandler : MonoBehaviour {
+
+	public const int TOTAL_ACTIVE_QUESTS = 3;
+
+	public static QuestEventHandler instance = null;
+
+	private List<Quest> quests = new List<Quest>();
+	
+	void Awake () {
+		if (instance == null) {
+			instance = this;
+		} else if (instance != this) {
+			Destroy (gameObject);
+		}
+	}
+
+	public bool CanAcceptQuest() {
+		return quests.Count < TOTAL_ACTIVE_QUESTS;
+	}
+
+	public void OnAcceptQuest(Quest quest) {
+		if (!CanAcceptQuest()) {
+			// TODO: throw error
+			return;
+		}
+		quests.Add(quest);
+	}
+
+	public void OnCompleteQuest(Quest quest) {
+		quests.Remove(quest);
+		quest = null;
+	}
+	
+	public void OnStealItem(Player player, NPC npc, Item item) {
+		foreach (Quest quest in quests) {
+			quest.OnStealItem(player, npc, item);
+		}
+	}
+
+	public void OnCraftItem(Player player, Item item) {
+		foreach (Quest quest in quests) {
+			quest.OnCraftItem(player, item);
+		}
+	}
+
+	public void OnDefeatedNPC(Player player, NPC npc) {
+		foreach (Quest quest in quests) {
+			quest.OnDefeatedNPC(player, npc);
+		}
+	}
+
+	public void OnSellItem(Player player, NPC npc, Item item) {
+		foreach (Quest quest in quests) {
+			quest.OnSellItem(player, npc, item);
+		}
+	}
+}
