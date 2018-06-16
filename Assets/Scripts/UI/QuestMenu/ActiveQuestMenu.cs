800c2b60c45e293e3c56dc6caab1c4c3416aedcb
Brandon Wang <sourbrandon@gmail.com>
2018-06-16 16:20:56 -0500

    CODE added quest detail object

---
+++ Assets/Scripts/UI/QuestMenu/ActiveQuestMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActiveQuestMenu : MonoBehaviour {
+
+	public static ActiveQuestMenu instance = null;
+
+	[SerializeField]
+	private GameObject questDetail;
+
+	void Awake () {
+		if (instance == null) {
+			instance = this;
+		} else {
+			Destroy(gameObject);
+		}
+	}
+	
+	public void Display(Quest[] activeQuests) {
+
+	}
+}
---
+++ Assets/Scripts/UI/QuestMenu/QuestDetail.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuestDetail : MonoBehaviour {
+
+	[SerializeField]
+	private Text npcNameText;
+	[SerializeField]
+	private Text questDetailsText;
+	[SerializeField]
+	private Text rewardText;
+
+	public void DisplayQuest(Quest quest) {
+		npcNameText.text = quest.reporter.GetName();
+		questDetailsText.text = quest.GetCurrentDetails();
+		rewardText.text = quest.GetCurrentReward().ToString();
+	}
+
+	public void DisplayQuestStage(QuestStage questStage, NPC reporter) {
+		npcNameText.text = reporter.GetName();
+		questDetailsText.text = questStage.GetDetails();
+		rewardText.text = questStage.GetReward().ToString();
+	}
+
+	public void DisplayEmptyQuest(NPC npc) {
+		npcNameText.text = npc.GetName();
+		questDetailsText.text = "No quest at this time right now.";
+		rewardText.text = "--";
+	}
+}
