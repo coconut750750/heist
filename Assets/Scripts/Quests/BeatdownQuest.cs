de1ee25afb17f2bbba42113c5197620bf6e07028
Brandon Wang <sourbrandon@gmail.com>
2018-06-27 19:16:15 -0500

    CODE added skeleton for beatdown quests

---
+++ Assets/Scripts/Quests/BeatdownQuest.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeatdownQuest : Quest {
+
+    public const int NUM_STAGES = 3;
+    private const string QUEST_DETAILS = "Beat up ";	
+
+    public BeatdownQuest(NPC reporter) : base(reporter, Constants.BEATDOWN_QUEST)
+    {
+    }
+
+    protected override QuestStage[] GenerateQuestStages() {
+        if (base.reporter == null) {
+            return null;
+        }
+
+        QuestStage[] stages = new QuestStage[NUM_STAGES];
+
+        
+
+        return stages;
+    }
+
+    public override void OnDefeatedNPC(NPC npc) {
+        if (GetCurrentStage().FulfillsRequirement(null, npc)) {
+            CompleteQuestStage();
+        }
+	}
+}
