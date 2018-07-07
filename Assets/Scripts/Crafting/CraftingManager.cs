6da9c06dbbece46ecfede3f8fccb792d6128d07c
Brandon Wang <sourbrandon@gmail.com>
2018-07-07 14:05:49 -0500

    CRAFTING preliminary crafting manager

---
+++ Assets/Scripts/Crafting/CraftingManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CraftingManager : MonoBehaviour {
+
+	public static CraftingManager instance = null;
+
+	public RecipeManager recipeManager = null;
+
+	public Item[] inputs;
+
+	void Awake() {
+		if (instance == null) {
+			instance = this;
+			recipeManager = new RecipeManager();
+		} else {
+			Destroy(gameObject);
+		}
+		print(recipeManager.ValidRecipe(inputs));
+	}
+
+	public Item TryCraft(Item[] items) {
+
+
+
+		return null;
+	}
+}
---
+++ Assets/Scripts/Crafting/RecipeManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class RecipeManager {
+
+	private List<Recipe> recipes;
+
+	public RecipeManager() {
+		recipes = new List<Recipe>();
+
+		Recipe recipe = new Recipe(new string[]{"Scrap Plastic","Scrap Plastic","Lighter"}, "Blank Card");
+		recipes.Add(recipe);
+	}
+
+	public bool ValidRecipe(Item[] requirements) {
+		foreach (Recipe recipe in recipes) {
+			if (recipe.IsValidRequirements(requirements.Select(item => item.itemName).ToArray())) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+}
