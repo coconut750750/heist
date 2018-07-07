b9344453582e2b9db552717b9cde046d6f8cd20b
Brandon Wang <sourbrandon@gmail.com>
2018-07-07 14:47:47 -0500

    CRAFTING crafting and undo working

--- Assets/Scripts/UI/PauseMenu/CraftingContent.cs
+++ Assets/Scripts/UI/PauseMenu/CraftingContent.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class CraftingContent : MonoBehaviour {
 
 	private CraftingStash craftingStash;
+	private Item[] undoSafety;
 
 	void Awake () {
 		craftingStash = gameObject.GetComponent<CraftingStash>();
@@ -16,9 +18,31 @@ public class CraftingContent : MonoBehaviour {
 
 	void OnDisable() {
 		craftingStash.Hide();
+		undoSafety = null;
 	}
 
 	public CraftingStash GetStash() {
 		return craftingStash;
+	}
+
+	public void TryCrafting() {
+		Item[] inputs = craftingStash.items.Where(item => item != null).ToArray();
+		undoSafety = inputs;
+
+		Item result = CraftingManager.instance.TryCraft(inputs);
+
+		if (result != null) {			
+			craftingStash.RemoveAll();
+			craftingStash.AddItemAtIndex(result, 3);
+		}
+	}
+
+	public void Undo() {
+		if (undoSafety != null) {
+			craftingStash.RemoveAll();
+			foreach (Item item in undoSafety) {
+				craftingStash.AddItem(item);
+			}
+		}
 	}
 }
