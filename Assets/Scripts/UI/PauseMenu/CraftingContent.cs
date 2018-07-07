66aa95698ac066eba74ea779402f0c1eb468c029
Brandon Wang <sourbrandon@gmail.com>
2018-07-07 14:36:27 -0500

    CRAFTING added crafting menu content

---
+++ Assets/Scripts/UI/PauseMenu/CraftingContent.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CraftingContent : MonoBehaviour {
+
+	private CraftingStash craftingStash;
+
+	void Awake () {
+		craftingStash = gameObject.GetComponent<CraftingStash>();
+	}
+
+	void OnEnable() {
+		craftingStash.Display();
+	}
+
+	void OnDisable() {
+		craftingStash.Hide();
+	}
+
+	public CraftingStash GetStash() {
+		return craftingStash;
+	}
+}
