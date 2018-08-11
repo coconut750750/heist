8c711ff43bbe9bb1fc114b1ce7ff6d44be315401
Brandon Wang <sourbrandon@gmail.com>
2018-08-11 17:32:43 -0500

    OTHER added previously ignored files

---
+++ Assets/Scripts/Managers/PoliceManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoliceManager : MonoBehaviour {
+
+	public int numSuspicious = 0;
+
+	private PoliceNPC[] police;
+
+	void Awake () {
+		police = GetComponentsInChildren<PoliceNPC>();
+	}
+
+	
+	
+}
